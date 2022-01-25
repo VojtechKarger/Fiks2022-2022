38c0e6679a66264dd2416df86092b492ab702346
vojta <vojta@ne.cz>
2022-01-25 12:23:02 +0100

    don't know why doesn't work

--- Fiks/main.swift
+++ Fiks/main.swift
@@ -27,21 +27,63 @@ func Krmeni() {
     let (N,Q) = (data[0],data[1])
     
     var graph = [Int: [Int]]()
-    
+    let values = LinkedList<Int>()
+    for i in 0..<N {
+        values.append(i + 1)
+    }
     for i in 1..<N {
         let value = input[i].components(separatedBy: .whitespaces).map(Int.toInt)
-        let arr = graph[value[0]] ?? []
-        graph[value[0]] = arr + [value[1]]
+        if let arr = graph[value[0]] {
+            graph[value[0]] = arr + [value[1]]
+        }else {
+            graph[value[0]] = [value[1]]
+            //values.append(value[0])
+        }
     }
-    for i in N...(N + Q) {
-        let row = input[i].components(separatedBy: .whitespaces).map(Int.toInt)
-        
-
+    
+    var output = ""
+    for i in N..<(N + Q) {
+        var k = input[i].components(separatedBy: .whitespaces).map(Int.toInt)
+        k.removeFirst()
+        let row = Set(k)
+        let basicVal = BFS_edited(1, graph, row)
+        var minVal = basicVal
+        values.forEach { value in
+            let val = BFS_edited(value, graph, row)
+            if val <= minVal {
+                minVal = val
+            }
+        }
+        print(minVal, basicVal)
+        output += String(abs(basicVal - minVal)) + "\n"
     }
+    print(output)
 }
-
-
 
 Krmeni()
 
 
+func BFS_edited(_ n: Int,_ graph: [Int: [Int]],_ row: Set<Int>) -> Int {
+    var row = row
+    var queue = Array<(Int,Int)>()
+    var i = 0
+    var result = 0
+    var visited = Set<Int>()
+    queue.append((n,i))
+    while !queue.isEmpty {
+        let curr = queue.removeFirst()
+        if i != curr.1 {
+            i += 1
+        }
+        if row.contains(curr.0) { result += i; row.remove(curr.0) }
+        guard let arr = graph[curr.0] else { continue }
+        for j in arr {
+            if !visited.contains(j) {
+                visited.insert(j)
+                queue.append((j,i + 1))
+            }
+        }
+        if row.isEmpty { break }
+    }
+    return result
+}
